b0f849bdb0a4b07a6510c1ab044dc1cfe24b28aa
Evellynkuuhaku <evellynkuuhaku@gmail.com>
2025-07-11 17:17:55 -0300

    Add files via upload

---
+++ primeiro-projeto/src/Desafio.java
@@ -0,0 +1,15 @@
+package Filme;
+
+public class Desafio {
+    public static void main(String[] args) {
+    System.out.println("Rei Leão");
+
+        int anodelancamento = 1998;
+        System.out.println("ano de lançamento: " + anodelancamento);
+        boolean incluindonoplano = true;
+        double notadofilme = 6.4;
+
+        double media = (5.6 + 5.7 + 7.9) / 3;
+        System.out.println(media);
+    }
+}
